2b9a428fd13b60d0ee79616c915d0e0536c299e8
Anna Wieteska <209614@edu.p.lodz.pl>
2021-07-30 15:26:02 +0200

    Add .sh file to run HANDS2019 dataset

---
+++ run_test_for_HANDS2019t.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# source ~/zzh/env/bin/activate
+
+dst_dir0=output/stage0
+dst_dir1=output/stage1
+result_path0=${dst_dir0}/result.txt
+result_path1=${dst_dir1}/result.txt
+
+# stage0
+echo 'stage0 start'
+python test_hands19task1.py \
+  --output_result_path ${result_path0} \
+  -v \
+  --input_test_img_folder dataset/HANDS19_Challenge/Task1/test_images \
+  --model_path weights/stage0.pth \
+  --gpu_id 0 \
+  --batch_size 128
+cd ${dst_dir0}
+zip -q result.txt.zip result.txt
+cd -
+echo 'stage0 done'
+
+# hand region augment
+echo 'hand region augmentation start'
+# single thread
+#python hand_region_augment.py \
+#  --joint_list_path ${dst_dir0}/result.txt \
+#  --dst_dir cache/hands19task1/test_images_augment \
+#  -v
+# multi thread
+mkdir -p cache1/part
+split -l 5000 ${result_path0} cache1/part/result.txt.part -d -a 2
+for ((i = 0; i < 25; i += 1)); do
+  index=$(echo $i | awk '{printf("%02d",$1)}')
+  python hand_region_augment.py \
+    -v \
+    --dst_dir cache1/hands19task1/test_images_augment \
+    --joint_list_path cache1/part/result.txt.part${index} &
+done
+wait
+# rm -rf cache1/part
+echo 'hand region augmentation done'
+
+# stage1
+echo 'stage1 start'
+python test_hands19task1.py \
+  --output_result_path ${result_path1} \
+  -v \
+  --input_test_img_folder cache/hands19task1/test_images_augment \
+  --model_path weights/stage1.pth \
+  --gpu_id 0 \
+  --batch_size 128
+cd ${dst_dir1}
+zip -q result.txt.zip result.txt
+echo 'stage1 done'
